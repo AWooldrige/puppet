c72fe9a21402a707da4879be538f6188afb392ed
Alistair Wooldrige <agw@woolie.co.uk>
2012-01-30 15:45:29 +0000

    Add tmux and vimrc configurations

--- manifests/sites.pp
+++ manifests/sites.pp
@@ -1,9 +1,49 @@
 node default {
+    user {"woolie":
+        ensure  => present,
+        uid     => '1500',
+        gid     => 'woolie',
+        groups  => 'woolie',
+        shell   => '/bin/bash',
+        home    => '/home/woolie',
+        managehome => true,
+    }
+    group { "woolie":
+        ensure  => present,
+        gid => '1500',
+    }
 
+
+    package { 'vim':
+        ensure => latest,
+    }
+    file { '/etc/vim/vimrc.local':
+        source  => 'puppet:///modules/vim/vimrc.local',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '644',
+        require => package['vim'],
+    }
+    package { 'tmux':
+        ensure => latest,
+    }
+    file { '/etc/tmux.conf':
+        source  => 'puppet:///modules/tmux/tmux.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '644',
+        require => package['tmux'],
+    }
+
+    $enhancers = [  "tree",
+                    "strace",
+                    "sudo" ]
+    package { $enhancers: ensure => "installed" }
 }
 node "metis.woolie.co.uk" {
 }
 
 node "agw-netbook" inherits default {
-
+}
+node "dev.local" inherits default {
 }
