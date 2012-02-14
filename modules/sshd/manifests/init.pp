3d8aa7702181232c286bceec2dae609a47b99c75
Alistair Wooldrige <agw@woolie.co.uk>
2012-02-14 18:22:30 +0000

    Add the ssh service to puppet, this wasn't before.@

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -9,7 +9,16 @@ class sshd {
         source  => 'puppet:///modules/sshd/sshd_config',
         owner   => 'root',
         group   => 'root',
-        mode    => '644',
-        require => [Package['openssh-server'], Group['sshallowedlogin']]
+        mode    => '600',
+        require => [Package['openssh-server'], Group['sshallowedlogin']],
+        notify  => Service['ssh']
     }
+    service { "ssh":
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        enable => true,
+        require => File['/etc/ssh/sshd_config']
+    }
+
 }
