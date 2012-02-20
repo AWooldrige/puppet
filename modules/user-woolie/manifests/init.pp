63bd83adb9f684d3fde0ad4fbb1e1dd20c52dd5b
Alistair Wooldrige <agw@woolie.co.uk>
2012-02-20 11:49:27 +0000

    Add user-woolie class, along with .gitconfig and .ssh/config files

---
+++ modules/user-woolie/manifests/init.pp
@@ -0,0 +1,37 @@
+class user-woolie {
+
+    # Make sure home directory exists
+    file { '/home/woolie':
+        ensure  => directory,
+        owner   => 'woolie',
+        group   => 'woolie',
+        mode    => '400',
+    }
+
+
+    # SSH configuration
+    file { '/home/woolie/.ssh':
+        ensure  => directory,
+        owner   => 'woolie',
+        group   => 'woolie',
+        mode    => '400',
+        require => File['/home/woolie']
+    }
+    file { '/home/woolie/.ssh/config':
+        source  => 'puppet:///modules/user-woolie/ssh/config',
+        owner   => 'woolie',
+        group   => 'woolie',
+        mode    => '400',
+        require => File['/home/woolie/.ssh']
+    }
+
+
+    # git configurations
+    file { '/home/woolie/.gitconfig':
+        source  => 'puppet:///modules/user-woolie/gitconfig',
+        owner   => 'woolie',
+        group   => 'woolie',
+        mode    => '400',
+        require => File['/home/woolie']
+    }
+}
