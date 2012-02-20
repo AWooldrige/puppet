1791b5e082ec947cbd5aa62084ee1fee180069d0
Alistair Wooldrige <agw@woolie.co.uk>
2012-02-20 16:09:31 +0000

    NTP  - Change servers to use ones from the NTP pool  - Remove all access restrictions  - Cron to remove

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -3,27 +3,31 @@ class ntp {
         ensure => latest
     }
     file { "/etc/timezone":
+        owner   => 'root',
+        group   => 'root',
+        mode    => '644',
         require => Package["tzdata"],
         source => "puppet:///modules/ntp/timezone",
-        notify => Exec['tzdata-reconfigure']
+        notify => Exec['tzdata-reconfigure'],
     }
     exec { 'tzdata-reconfigure':
         command => 'dpkg-reconfigure -f noninteractive tzdata',
         path => [
             '/usr/local/bin',
             '/opt/local/bin',
-            '/usr/bin', 
-            '/usr/sbin', 
+            '/usr/bin',
+            '/usr/sbin',
             '/bin',
-            '/sbin'],
-        logoutput => true
+            '/sbin']
     }
 
     package { "ntp":
         ensure => latest
     }
     file { "/etc/ntp.conf":
-        mode    => "644",
+        owner   => 'root',
+        group   => 'root',
+        mode    => '644',
         source => 'puppet:///modules/ntp/ntp.conf',
         notify  => Service["ntp"],
         require => Package["ntp"],
@@ -32,5 +36,13 @@ class ntp {
         ensure  => running,
         enable  => true,
         require => Package["ntp"],
+    }
+
+    cron { 'ntp-kick' :
+        command => "/usr/bin/ntpq -np | /bin/grep '^\\*' > /dev/null || ( /usr/bin/ntpq -np ; /etc/init.d/ntp restart )",
+        user    => 'root',
+        ensure  => present,
+        minute  => [0],
+        require => Package['ntp']
     }
 }
