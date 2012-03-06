41dde9b13589567a6f7e20c43a00a96da2b3d363
Alistair Wooldrige <agw@woolie.co.uk>
2012-03-06 17:45:59 +0000

    puppet-auto-update:  - Ensure old puppet-auto-update cron job and script is removed  - Add new cron job to run puppet every hour regardless of git change

--- modules/puppet-auto-update/manifests/init.pp
+++ modules/puppet-auto-update/manifests/init.pp
@@ -1,9 +1,17 @@
 class puppet-auto-update {
-    cron { 'puppet-auto-update':
+    cron { 'puppet-git-update':
         command => '/usr/bin/puppet-git-update',
         user    => root,
         ensure => present,
         minute  => [0, 10, 20, 30, 40, 50],
+        require => [ File['/usr/bin/puppet-run'],
+                     File['/usr/bin/puppet-git-update'] ]
+    }
+    cron { 'puppet-run':
+        command => '/usr/bin/puppet-run',
+        user    => root,
+        ensure => present,
+        minute  => [55],
         require => [ File['/usr/bin/puppet-run'],
                      File['/usr/bin/puppet-git-update'] ]
     }
@@ -19,5 +27,20 @@ class puppet-auto-update {
         owner => 'root',
         group => 'root',
         mode => '744'
+    }
+
+
+    ####t
+    # Remove old cron jobs and file
+    ##
+    cron { 'puppet-auto-update':
+        command => "/usr/share/puppet-auto-update",
+        user    => root,
+        ensure => absent,
+        minute  => [0, 10, 20, 30, 40, 50],
+    }
+
+    file { "/usr/share/puppet-auto-update":
+        ensure => absent
     }
 }
