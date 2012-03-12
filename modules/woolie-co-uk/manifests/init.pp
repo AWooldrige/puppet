a7d09e84464ed8749d7ac764562bd6de8e640aeb
Alistair Wooldrige <agw@woolie.co.uk>
2012-03-12 21:00:13 +0000

    Start woolie-co-uk module with a directory an httpd conf

--- modules/woolie-co-uk/manifests/init.pp
+++ modules/woolie-co-uk/manifests/init.pp
@@ -1,2 +1,19 @@
 class woolie-co-uk () {
+    file { '/var/www/woolie-co-uk':
+        ensure  => directory,
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '700',
+    }
+    file { "/etc/apache2/sites-available/woolie-co-uk":
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '400',
+        content => template("woolie-co-uk/conf"),
+        notify  => Service['apache2'],
+    }
+
+    httpd::site { 'woolie-co-uk':
+        ensure => enabled
+    }
 }
