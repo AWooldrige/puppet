ab68da9db43d5fc135ad38e428e4e263b5c67efd
Alistair Wooldrige <agw@woolie.co.uk>
2012-04-13 17:02:35 +0100

    Move the default configuration out of sites.pp into its own module

--- manifests/sites.pp
+++ manifests/sites.pp
@@ -23,52 +23,8 @@ $extlookup_datadir = "/root/extlookup/"
 $extlookup_precedence = ["nodes/%{hostname}", "common"]
 
 node default {
-    $enhancers = [
-        'tree',
-        'strace',
-        'ack-grep',
-        'iotop',
-        'man-db',
-        'makepasswd',
-        'curl'
-    ]
-    $notneeded = [
-        'ack'
-    ]
-    package { $enhancers: ensure => installed }
-    package { $notneeded: ensure => purged }
-
-    # Alias ack to ack-grep
-    exec { "/bin/ln -sf /usr/bin/ack-grep /usr/local/bin/ack":
-        unless => "/bin/sh -c '[ -L /usr/local/bin/ack ]'",
-        require => Package['ack-grep']
-    }
-
-    file { '/root/extlookup':
-        ensure  => directory,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '400',
-    }
-    file { '/root/extlookup/common.csv':
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '400',
-    }
-
-    include user-woolie
-    include puppet-auto-update
-    include ntp
-    include tmux
-    include vim
-    include sudo
-    include sshd
-    include bash
+    include default-config
 }
-
-
-
 
 #########################################################################
 ##                             Desktop Nodes                           ##
---
+++ modules/default-config/manifests/init.pp
@@ -0,0 +1,44 @@
+class default-config {
+
+    $enhancers = [
+        'tree',
+        'strace',
+        'ack-grep',
+        'iotop',
+        'man-db',
+        'curl'
+    ]
+    $notneeded = [
+        'ack'
+    ]
+    package { $enhancers: ensure => installed }
+    package { $notneeded: ensure => purged }
+
+    # Alias ack to ack-grep
+    exec { "/bin/ln -sf /usr/bin/ack-grep /usr/local/bin/ack":
+        unless => "/bin/sh -c '[ -L /usr/local/bin/ack ]'",
+        require => Package['ack-grep']
+    }
+
+    file { '/root/extlookup':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '400',
+    }
+    file { '/root/extlookup/common.csv':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '400',
+    }
+
+    include user-woolie
+    include puppet-auto-update
+    include ntp
+    include tmux
+    include vim
+    include sudo
+    include sshd
+    include bash
+}
