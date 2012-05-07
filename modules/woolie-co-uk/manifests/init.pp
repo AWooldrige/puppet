09eab4ca32972794203e86855e1a7b17fcca58fa
Alistair Wooldrige <agw@woolie.co.uk>
2012-05-07 16:18:18 +0100

    Up version number of woolie-co-uk to wordpress 3.3.2

--- modules/woolie-co-uk/manifests/init.pp
+++ modules/woolie-co-uk/manifests/init.pp
@@ -1,6 +1,6 @@
 class woolie-co-uk () {
     wordpress::instance {'wooliecouk':
-        ensure  => "3.3.1",
+        ensure  => "3.3.2",
         path    => "/var/www/woolie-co-uk",
         domain  => "woolie.co.uk",
         backups => true,
