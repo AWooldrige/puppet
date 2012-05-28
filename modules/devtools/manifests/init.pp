6d1dfbf2ac66e801bf2e10714c2198b7851933a1
Alistair Wooldrige <agw@woolie.co.uk>
2012-05-28 22:07:27 +0100

    Remove phing installation, but add build-essential

--- modules/devtools/manifests/init.pp
+++ modules/devtools/manifests/init.pp
@@ -3,6 +3,9 @@ class devtools {
     package { 'php-pear':
         ensure => installed
     }
+    package { 'build-essential':
+        ensure => installed
+    }
 
     # PHPUnit can't be installed with apt, as it doesn't come
     # with all dependancies
@@ -21,19 +24,6 @@ class devtools {
         require => [ Package['phpunit'],
                      Package['php-pear'] ],
         unless => 'pear list -c phpunit && pear list -c phpunit | grep PHPUnit'
-    }
-
-    exec { 'phing-install':
-        command => 'pear upgrade pear;pear channel-discover pear.phing.info;pear install --alldeps phing/phing;',
-        path => [
-            '/usr/local/bin',
-            '/opt/local/bin',
-            '/usr/bin',
-            '/usr/sbin',
-            '/bin',
-            '/sbin'],
-        require => Package['php-pear'],
-        unless => 'pear list -c phing && pear list -c phing | grep phing'
     }
 
     package { 'php-codesniffer':
