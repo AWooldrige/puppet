9e9e766974effa1eceee385227c36d79a2272ebe
Alistair Wooldrige <agw@woolie.co.uk>
2012-07-19 15:24:23 +0100

    Convert motd script into a python script

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -16,7 +16,7 @@ class motd {
     file { [
         "/etc/update-motd.d/00-header",
         "/etc/update-motd.d/10-help-text",
-        "/etc/update-motd.d/50-landscape-sysinfo",
+        "/usr/share/landscape/landscape-sysinfo.wrapper",
         "/etc/update-motd.d/90-updates-available",
         "/etc/update-motd.d/91-release-upgrade",
         "/etc/update-motd.d/98-fsck-at-reboot",
