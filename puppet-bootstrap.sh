f9507717c50d275d2ae5d3bd5517547c47f795cd
Alistair Wooldrige <agw@woolie.co.uk>
2012-10-16 21:09:18 +0100

    Remove http_port, https_port, http_host and varnish_port from the bootstrap

--- puppet-bootstrap.sh
+++ puppet-bootstrap.sh
@@ -5,11 +5,7 @@
 
 extlookupcsvfile='/root/extlookup/common.csv'
 
-fields=( "httpd/http_port,<port for apache to list on - e.g. 81>"
-         "httpd/https_port,<port for SSL apache to listen on - e.g. 443>"
-         "varnish/httpd_http_host,<host to retrieve content - e.g. 127.0.0.1>"
-         "varnish/port,<port for varnish to listen on - e.g. 80>"
-         "backup/path,<remote path for backups - e.g. backup8::/var/bkps or /var/bkps>"
+fields=( "backup/path,<remote path for backups - e.g. backup8::/var/bkps or /var/bkps>"
          "ssmtp/mailhub,<SMTP mail address - e.g. smtp.gmail.com:587>"
          "ssmtp/user,<SMTP user name - e.g. noreply@woolie.co.uk>"
          "ssmtp/pass,<SMTP password>" )
