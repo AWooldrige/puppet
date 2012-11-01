6700358a5002ebbf2fd67aafe29463c1c805dd93
Alistair Wooldrige <agw@woolie.co.uk>
2012-11-01 20:11:23 +0000

    Adding the new ssh public key to backupincoming user and setting password has to random string

--- modules/user-backupincoming/manifests/init.pp
+++ modules/user-backupincoming/manifests/init.pp
@@ -6,6 +6,7 @@ class user-backupincoming {
         groups  => ['backupincoming', 'sshallowedlogin'],
         shell   => '/bin/bash',
         home    => '/home/backupincoming',
+        password => generate('/root/getpassword', 'user_backupincoming'),
         managehome => true
     }
     group { "backupincoming":
@@ -31,7 +32,7 @@ class user-backupincoming {
     ssh_authorized_key { "backupincoming":
         ensure => "present",
         type => "ssh-rsa",
-        key => "AAAAB3NzaC1yc2EAAAADAQABAAACAQDGoNr/PhMh2XEjsEgEGQApvcnp7AKTDyHt3A97OQTdcZ+lG5Ywx4jDpymsKU6qw9yxpElZVNIP/HLiJlfVzRlF636PIjtAyZtaoujeQGuaGp+YalqaD/YKD2LlqqHZZnsxv63IPHy7NFvpasJdHlRlaGBdLfpqvMIGgQOYlHK3M9+UDcMBK1EpftzM4x1VqGY9exs0d8bqQtIBwi7/3gVK+FymG6LM3pYnAMuCIs98T/6rwbysxV7ojogWOI5mX7uPrQhwz65AUn6PlB2LhNPhSYUAjWGePlEXG7vNMSPXyX51ZZwn4q7gAgxN9H5zi7nYQATstY6jwX7He88pvoBYxsogRQcp9TaN3gYtdEbW/HbS4v2qp2H+OuhXnMAnmejZ5Sl5FFxJIEFhbaXxBk71B9+7uX+4I3la5PZ6k7jcJMNy/RLs6xW2fBEVplMjZJQcFbmDopq03/H+v0lsXOYLdBbTF2lHadeyHNtPLeGstYWHutGsMMqSyKLF/lLgJ1tPWsj9xOAsh+iZtczmr8Wb4updr9qWw8xef5EzDyPS3RfEdYCuQ5GrwzHxWmhdK2JG998a0sv87xMGVD7D6J50jYSvgGYqbIXM6VlOfrrS/fD0Y3wBeQDnCLB7lay+USA06YTvwrvw2RxIkKSe2jseLM47nxGAjSLUj58rRyz5GQ==",
+        key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBRA1l8X79XY+YAdwerRNYiqk0Xt5S7B4U5tB1LUksiuxAAntHdFLzPfoHFeKK5b274PGdYnKRmFluVnvIjAZhxjfpbGQViFV0yv65ZTRXVBUvwPKFxYlZHFz6qOChGs114voxUwCXKNJv3u3Re0Ofl6h8ei1ZDq8K9x8J6eapSMrpSoqy7E1xpQd8d3LqXgep5n5rug49378fx+ZLG2RIovRcznvW0N2kquQP7pkK1gsgfynIfsHK7n9a8Oc4gvDBCA/PRXfFt44KWpdmJZhv4all5G+SPLB4dpcWQieNO1bCUwTBFaV0GeTOAfPbkcnyhbfkB3agYqM3AnVWwIp",
         user => "backupincoming",
         require => [ File['/home/backupincoming/.ssh'] ]
     }
