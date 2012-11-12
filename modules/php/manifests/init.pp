14586e680b2fab836697398df14c0e25b9a1f267
Alistair Wooldrige <agw@woolie.co.uk>
2012-11-12 16:44:16 +0000

    Adding APC, using the default parameters

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
 
-    package { ["libapache2-mod-php5", "php5-mysql", "php5-gd"]:
+    package { ['libapache2-mod-php5', 'php5-mysql', 'php5-gd', 'php-apc']:
         ensure => installed
     }
 
