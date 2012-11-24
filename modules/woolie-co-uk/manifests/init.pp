8b9831eceaf794df171adbf1b34e97a0991f45cd
Alistair Wooldrige <agw@woolie.co.uk>
2012-11-24 11:44:05 +0000

    Upgrade woolie.co.uk WordPress and add plugins and theme

--- modules/woolie-co-uk/manifests/init.pp
+++ modules/woolie-co-uk/manifests/init.pp
@@ -2,15 +2,33 @@ class woolie-co-uk {
 
     $wp_id = 'wooliecouk'
 
+    $themezip = 'old-wooliecouk-theme.zip'
+    exec { "download-${themezip}-theme":
+        command => "curl -L -o /opt/local-zips/${themezip} https://github.com/downloads/AWooldrige/puppet/${themezip}",
+        creates => "/opt/local-zips/${themezip}",
+        require => File['/opt/local-zips']
+    }
+
+
     wordpress::instance { $wp_id:
-        ensure  => "3.3.2",
+        ensure  => "3.4.2",
         domain  => "woolie.co.uk",
         backups => true,
         http_port => 81
     }
 
-    wordpress::plugin { "${wp_id}:akismet":
+    wordpress::plugin { [ "${wp_id}:akismet",
+                          "${wp_id}:disqus-comment-system",
+                          "${wp_id}:google-analytics-for-wordpress",
+                          "${wp_id}:wp-syntax" ]:
         ensure => 'installed',
         require => Wordpress::Instance[$wp_id]
+    }
+
+    wordpress::theme { "${wp_id}:woolie.co.uk":
+        ensure => 'installed',
+        active => true,
+        source_file => "/opt/local-zips/${themezip}",
+        require => [ Wordpress::Instance[$wp_id], Exec["download-${themezip}-theme"] ]
     }
 }
