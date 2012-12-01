5e771e6f5acec9ac338cff7a4cf8298a0721fe9d
Alistair Wooldrige <agw@woolie.co.uk>
2012-12-01 14:29:09 +0000

    Adding plugins and update WordPress for onmyplate.co.uk

--- modules/onmyplate-co-uk/manifests/init.pp
+++ modules/onmyplate-co-uk/manifests/init.pp
@@ -1,8 +1,20 @@
-class onmyplate-co-uk () {
-    wordpress::instance {'ompcouk':
-        ensure  => "3.3.2",
+class onmyplate-co-uk {
+
+    $wp_id = 'ompcouk'
+
+    wordpress::instance { $wp_id:
+        ensure  => "3.4.2",
         domain  => "onmyplate.co.uk",
         backups => true,
         http_port => 81
+    }
+
+    wordpress::plugin { [ "${wp_id}:akismet",
+                          "${wp_id}:disqus-comment-system",
+                          "${wp_id}:google-analytics-for-wordpress",
+                          "${wp_id}:google-sitemap-generator" ]:
+        ensure => 'installed',
+        active => true,
+        require => Wordpress::Instance[$wp_id]
     }
 }
