95081483a7252266c969813531593a16014b76fe
Alistair Wooldrige <agw@woolie.co.uk>
2012-12-19 17:12:21 +0000

    Documentation tweaks to wp-varnish.php

--- modules/wordpress/files/wp-varnish.php
+++ modules/wordpress/files/wp-varnish.php
@@ -9,25 +9,12 @@ Author URI: http://woolie.co.uk
 License: GPLv2 or later
 
 What does this plugin do:
- * If a theme's name is suffixed with "-dev", then Cache-Control headers will
-   be sent to prevent caching in Varnish and in the browser.
-
-*/
-
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Cache-Control headers:
+    * Suitable caching applied to different pages
+    * If a theme's name is suffixed with "-dev", then Cache-Control headers will
+      be sent to prevent caching in Varnish and in the browser.
+ * When a post is modified, it will request that Varnish purge that page, and
+   the homepage
 */
 
 define('WP_VARNISH_PLUGIN_VERSION', '0.1.2');
