41be21b7dbf5784f3e8b26a021b1653f0a1433f6
Alistair Wooldrige <agw@woolie.co.uk>
2013-01-21 20:39:50 +0000

    Update OnMyPlate.co.uk plugin and theme versions

--- modules/onmyplate-co-uk/manifests/init.pp
+++ modules/onmyplate-co-uk/manifests/init.pp
@@ -2,16 +2,16 @@ class onmyplate-co-uk {
 
     $wp_id = 'ompcouk'
 
-    $theme_version = '0.1.0'
+    $theme_version = '0.2.0'
     exec { "download-omp-theme-${theme_version}":
-        command => "curl -L -o /opt/local-zips/omp-theme-${theme_version}.zip https://dl.dropbox.com/s/xsqjfy1logosbmg/omp-theme-${theme_version}.zip?dl=1",
+        command => "curl -L -o /opt/local-zips/omp-theme-${theme_version}.zip https://dl.dropbox.com/s/gm2rep7exbdxq4m/omp-theme-${theme_version}.zip?dl=1",
         creates => "/opt/local-zips/omp-theme-${theme_version}.zip",
         require => File['/opt/local-zips']
     }
 
-    $plugin_version = '0.1.0'
+    $plugin_version = '0.2.0'
     exec { "download-omp-plugin-${plugin_version}":
-        command => "curl -L -o /opt/local-zips/omp-plugin-${plugin_version}.zip https://dl.dropbox.com/s/2plzzb89m2q3a2i/omp-plugin-${plugin_version}.zip?dl=1",
+        command => "curl -L -o /opt/local-zips/omp-plugin-${plugin_version}.zip https://dl.dropbox.com/s/tce6rjijpuvl0vz/omp-plugin-${plugin_version}.zip?dl=1",
         creates => "/opt/local-zips/omp-plugin-${plugin_version}.zip",
         require => File['/opt/local-zips']
     }
@@ -31,12 +31,17 @@ class onmyplate-co-uk {
             Wordpress::Instance[$wp_id],
             Exec["download-omp-theme-${theme_version}"] ]
     }
+    wordpress::plugin { "${wp_id}:omp-plugin-0.1.0":
+        ensure => "removed",
+        require => [ Wordpress::Instance[$wp_id] ]
+    }
     wordpress::plugin { "${wp_id}:omp-plugin-${plugin_version}":
         ensure => 'installed',
         active => true,
         source_file => "/opt/local-zips/omp-plugin-${plugin_version}.zip",
         require => [
             Wordpress::Instance[$wp_id],
+            Wordpress::Plugin["${wp_id}:omp-plugin-0.1.0"],
             Exec["download-omp-plugin-${plugin_version}"] ]
     }
 }
