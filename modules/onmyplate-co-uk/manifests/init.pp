8d0e7c28697eabb4cbf4a0d9898e13ec8ab5513b
Alistair Wooldrige <agw@woolie.co.uk>
2013-02-27 11:40:52 +0000

    Updating to latest version of plugin and theme OnMyPlate.co.uk

--- modules/onmyplate-co-uk/manifests/init.pp
+++ modules/onmyplate-co-uk/manifests/init.pp
@@ -2,14 +2,14 @@ class onmyplate-co-uk {
 
     $wp_id = 'ompcouk'
 
-    $theme_version = '0.3.0'
+    $theme_version = '0.4.0'
     exec { "download-omp-theme-${theme_version}":
         command => "curl -L -o /opt/local-zips/omp-theme-${theme_version}.zip https://s3-eu-west-1.amazonaws.com/woolie-releases/wp_ompcouk/omp-theme-${theme_version}.zip",
         creates => "/opt/local-zips/omp-theme-${theme_version}.zip",
         require => File['/opt/local-zips']
     }
 
-    $plugin_version = '0.2.0'
+    $plugin_version = '0.3.0'
     exec { "download-omp-plugin-${plugin_version}":
         command => "curl -L -o /opt/local-zips/omp-plugin-${plugin_version}.zip https://s3-eu-west-1.amazonaws.com/woolie-releases/wp_ompcouk/omp-plugin-${plugin_version}.zip",
         creates => "/opt/local-zips/omp-plugin-${plugin_version}.zip",
@@ -31,9 +31,12 @@ class onmyplate-co-uk {
             Wordpress::Instance[$wp_id],
             Exec["download-omp-theme-${theme_version}"] ]
     }
-    wordpress::plugin { "${wp_id}:omp-plugin-0.1.0":
+    wordpress::plugin { [
+            "${wp_id}:omp-plugin-0.1.0",
+            "${wp_id}:omp-plugin-0.2.0"]:
         ensure => "removed",
-        require => [ Wordpress::Instance[$wp_id] ]
+        require => Wordpress::Instance[$wp_id],
+        before => Wordpress::Theme["${wp_id}:omp-theme-${theme_version}"]
     }
     wordpress::plugin { "${wp_id}:omp-plugin-${plugin_version}":
         ensure => 'installed',
