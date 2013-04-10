122874299cb519d953a25ee4279029f5e04f48d6
Alistair Wooldrige <agw@woolie.co.uk>
2013-04-10 22:16:56 +0100

    Tidy up and improvement of motd module

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,37 +1,37 @@
 class motd {
-    file { "/etc/issue":
-        owner   => 'root',
-        group   => 'root',
-        mode    => '644',
-        content => template("motd/issue"),
+    file { '/etc/issue':
+        source => 'puppet:///modules/motd/issue',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644'
     }
-    file { "/etc/issue.net":
-        owner   => 'root',
-        group   => 'root',
-        mode    => '644',
-        content => template("motd/issue"),
+    file { '/etc/issue.net':
+        source => 'puppet:///modules/motd/issue',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644'
     }
 
     # Disable the current MOTD scripts
     file { [
-        "/etc/update-motd.d/00-header",
-        "/etc/update-motd.d/10-help-text",
-        "/usr/share/landscape/landscape-sysinfo.wrapper",
-        "/etc/update-motd.d/90-updates-available",
-        "/etc/update-motd.d/91-release-upgrade",
-        "/etc/update-motd.d/98-fsck-at-reboot",
-        "/etc/update-motd.d/98-reboot-required",
-        "/etc/update-motd.d/99-footer"
+        '/etc/update-motd.d/00-header',
+        '/etc/update-motd.d/10-help-text',
+        '/usr/share/landscape/landscape-sysinfo.wrapper',
+        '/etc/update-motd.d/90-updates-available',
+        '/etc/update-motd.d/91-release-upgrade',
+        '/etc/update-motd.d/98-fsck-at-reboot',
+        '/etc/update-motd.d/98-reboot-required',
+        '/etc/update-motd.d/99-footer'
         ]:
-        owner   => 'root',
-        group   => 'root',
-        mode    => '644'
+        owner => 'root',
+        group => 'root',
+        mode  => '0644'
     }
 
-    file { "/etc/update-motd.d/00-custom-motd":
+    file { '/etc/update-motd.d/00-custom-motd':
+        content => template('motd/motd'),
         owner   => 'root',
         group   => 'root',
-        mode    => '755',
-        content => template("motd/motd")
+        mode    => '0755'
     }
 }
