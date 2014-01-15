1a240496676a1205c846de68b26418ab939e927e
Alistair Wooldrige <agw@woolie.co.uk>
2014-01-15 17:38:56 +0000

    Improving the logging library:
    * Logs all uncaught exceptions
    * All scripts using the library now handle exception logging in a
      uniform way.

--- modules/python/files/woolielibs/loghelper.py
+++ modules/python/files/woolielibs/loghelper.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from logging.handlers import SysLogHandler
 from os import getpid
@@ -8,18 +9,24 @@ def bootstrap_logger(name):
     l = logging.getLogger(name)
     l.setLevel(logging.DEBUG)
 
+    #Catchall for all uncaught Exceptions
+    def handleUncaughtException(excType, excValue, traceback):
+        l.error("Uncaught exception", exc_info=(excType, excValue, traceback))
+    sys.excepthook = handleUncaughtException
+
+    prefix = "{0}[{1}] ".format(name, str(getpid()))
+
     #Console handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    ch.setFormatter(logging.Formatter(LOG_FORMAT))
+    ch.setFormatter(logging.Formatter(prefix + LOG_FORMAT))
     l.addHandler(ch)
 
     #Syslog handler
     sh = SysLogHandler(address='/dev/log')
     sh.setLevel(logging.DEBUG)
-    prefix = "{0}[{1}] ".format(name.upper(), str(getpid()))
     sh.setFormatter(logging.Formatter(prefix + LOG_FORMAT))
     l.addHandler(sh)
 
-    l.debug("Starting {0} script...".format(name))
+    l.debug("Starting {0} script.".format(name))
     return l
