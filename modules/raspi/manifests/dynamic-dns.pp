93c9769b72734a5f4f1c2a5f9efc10b767ea1a38
Alistair Wooldrige <agw@woolie.co.uk>
2014-01-22 21:48:01 +0000

    Using STUN for external IP determination

--- modules/raspi/manifests/dynamic-dns.pp
+++ modules/raspi/manifests/dynamic-dns.pp
@@ -2,16 +2,22 @@ class raspi::dynamic-dns {
     package { 'python-dnspython':
         ensure => installed
     }
+    package { 'pystun':
+        ensure   => installed,
+        provider => pip
+    }
     file { '/usr/bin/update-dynamic-dns':
         source  => 'puppet:///modules/raspi/update-dynamic-dns',
         owner   => 'root',
         group   => 'root',
-        mode    => '0755'
+        mode    => '0755',
+        require => [ Package['pystun'], Package['python-dnspython'] ]
     }
     cron {"update-dynamic-dns":
         ensure  => present,
         command => "/usr/bin/chronic /usr/bin/update-dynamic-dns",
         user    => root,
-        minute  => [0, 10, 20, 30, 40, 50]
+        minute  => [0, 10, 20, 30, 40, 50],
+        require => File['/usr/bin/update-dynamic-dns']
     }
 }
