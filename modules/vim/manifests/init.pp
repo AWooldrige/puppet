44254e81c1d0139e3c742b99443e9a9c4c03aaff
Alistair Wooldrige <agw@woolie.co.uk>
2014-02-09 22:27:59 +0000

    Removing manually supplied vim plugins to move to the vundle plugin manager

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -13,4 +13,23 @@ class vim {
         force   => true,
         require => Package['vim']
     }
+
+    file { ['/home/woolie/.vim/bundle', '/home/woolie/.vim/backup',
+           '/home/woolie/.vim/tmp', '/home/woolie/.vim/undo']:
+        ensure => directory,
+        owner  => 'woolie',
+        group  => 'woolie',
+        mode   => '0755'
+    }
+    exec { 'install-vundle':
+        command     => 'git clone https://github.com/gmarik/vundle.git /home/woolie/.vim/bundle/vundle',
+        path        => [ '/usr/bin', '/bin', '/usr/local/bin' ],
+        require     => [
+            Package['git', 'vim'],
+            File['/home/woolie/.vim/bundle']],
+        logoutput   => 'true',
+        user        => 'woolie',
+        tries       => 3,
+        creates     => '/home/woolie/.vim/bundle/vundle'
+    }
 }
