b45370aa1ada1c6fc97f96ff7aa4c04fb9d734d0
Alistair Wooldrige <agw@woolie.co.uk>
2014-10-16 22:24:22 +0100

    Tidying up puppet-github-update

--- modules/puppet-auto-update/manifests/init.pp
+++ modules/puppet-auto-update/manifests/init.pp
@@ -1,45 +1,32 @@
 class puppet-auto-update {
 
-    file { '/usr/bin/puppet-git-run':
-        source => 'puppet:///modules/puppet-auto-update/puppet-git-run',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0540'
+    file { ['/usr/bin/puppet-git-run',
+            '/usr/bin/puppet-git-update',
+            '/etc/logrotate.d/puppet-git']:
+        ensure => absent
     }
-    file { '/usr/bin/puppet-git-update':
-        source => 'puppet:///modules/puppet-auto-update/puppet-git-update',
+    file { '/usr/bin/puppet-github-update':
+        source => 'puppet:///modules/puppet-auto-update/puppet-github-update',
         owner  => 'root',
         group  => 'root',
-        mode   => '0540'
+        mode   => '0755'
     }
 
-    cron { 'puppet-git-update':
-        command => '/usr/bin/chronic /usr/bin/puppet-git-update',
+    cron { ['puppet-git-update', 'puppet-git-run']:
+        ensure => absent
+    }
+    cron { 'puppet-github-update':
+        command => '/usr/bin/chronic /usr/bin/puppet-github-update',
         user    => root,
         ensure  => present,
         hour    => 9,
         minute  => 0,
-        require => [ File['/usr/bin/puppet-git-run'],
-                     File['/usr/bin/puppet-git-update'] ]
-    }
-    cron { 'puppet-git-run':
-        command => '/usr/bin/chronic /usr/bin/puppet-git-run',
-        user    => root,
-        ensure  => present,
-        hour    => 21,
-        minute  => 0,
-        require => [ File['/usr/bin/puppet-git-run'],
-                     File['/usr/bin/puppet-git-update'] ]
+        require => File['/usr/bin/puppet-github-update']
     }
 
-    file { '/var/log/puppet-git':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-    file { '/etc/logrotate.d/puppet-git':
-        source => 'puppet:///modules/puppet-auto-update/etc/logrotate.d/puppet-git',
+    file { '/etc/logrotate.d/puppet-github-update':
+        source =>
+        'puppet:///modules/puppet-auto-update/etc/logrotate.d/puppet-github-update',
         owner  => 'root',
         group  => 'root',
         mode   => '0644'
