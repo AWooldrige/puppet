f55675b8e2fd219355fefd8ba0fe7f79e5c7b3ae
Alistair Wooldrige <alistair@wooldrige.co.uk>
2020-07-08 22:43:44 +0100

    Add epaper watchanddisplay

--- modules/raspi/manifests/epaperdisplay.pp
+++ modules/raspi/manifests/epaperdisplay.pp
@@ -1,6 +1,33 @@
 class raspi::epaperdisplay {
 
-    package { ['python3-pil', 'python3-numpy']:
+    exec { 'daemon-reload':
+        command => '/usr/bin/systemctl daemon-reload',
+        refreshonly => true
+    }
+
+    package { ['python3-pil', 'python3-numpy', 'python3-watchdog']:
         ensure => installed
+    } ->
+    exec { 'Clone waveshare ePaper repo':
+       command => '/usr/bin/git clone https://github.com/waveshare/e-Paper /opt/epaperlibs',
+       creates => '/opt/epaperlibs/README.md',
+       require => Package['git']
+    } ->
+    file { '/usr/local/bin/watchanddisplay':
+        source => 'puppet:///modules/raspi/epaperdisplay/watchanddisplay',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    } ->
+    file { "/etc/systemd/system/watchanddisplay.service":
+        source => 'puppet:///modules/raspi/epaperdisplay/watchanddisplay.service',
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        notify => Exec['daemon-reload']
+    } ->
+    service { 'watchanddisplay':
+        ensure => running,
+        enable => true
     }
 }
